5cb1953210ee04b0dfe7581195de0069ce7d2617
Amine derbal <aminedoran31@gmail.com>
2023-09-14 19:10:29 +0100

    Add constants data

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+import {
+  arrowSync, checkmark, mdiCards, phTruck,
+} from '../assets';
+
+const claims = [
+  {
+    id: 1,
+    claim: '30-DAY SATISFACTION GUARANTEE',
+    logo: checkmark,
+  },
+  {
+    id: 2,
+    claim: 'FREE DELIVERY ON ORDERS OVER $40.00',
+    logo: phTruck,
+  },
+  {
+    id: 3,
+    claim: '50.000 HAPPY CUSTOMERS',
+    logo: mdiCards,
+  },
+  {
+    id: 4,
+    claim: '100% MONEY BACK GUARANTEE',
+    logo: arrowSync,
+  },
+];
+
+export default claims;
