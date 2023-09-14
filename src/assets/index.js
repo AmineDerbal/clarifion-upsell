0aa3565f83897f3bebc251278cb877b403f17954
Amine derbal <aminedoran31@gmail.com>
2023-09-14 19:10:00 +0100

    Config the import and exports for the assets

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import arrowSync from './fluent_arrow-sync-checkmark-20-regular.png';
+import checkmark from './fluent_checkmark-starburst-20-regular.png';
+import mdiCards from './mdi_cards-heart-outline.png';
+import phTruck from './ph_truck-light.png';
+import clarifionLogo from './600a41c73b670a97ae1d4f47_Clarifion_Logo 1.png';
+import mcAfee from './McAfee_Secure.png';
+import norton from './norton-antivirus-logo 1.png';
+
+export {
+  arrowSync, checkmark, mdiCards, phTruck, clarifionLogo, mcAfee, norton,
+};
