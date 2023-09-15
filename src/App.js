55e3db639658779e255788dd630fcc6dec5da252
Amine derbal <aminedoran31@gmail.com>
2023-09-15 17:20:01 +0100

    Update App to call components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './components/Header';
+import Product from './components/Product';
+import Footer from './components/Footer';
 
 const App = () => (
-  <>
+  <div className="manrope">
     <Header />
-  </>
+    <Product />
+    <Footer />
+  </div>
 );
 
 export default App;
