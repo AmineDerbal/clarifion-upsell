ef58459acb8308aeb673889cbb0630d127b48e1b
Amine derbal <aminedoran31@gmail.com>
2023-09-15 17:15:40 +0100

    Update import and exports for assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,45 @@ import phTruck from './ph_truck-light.png';
 import clarifionLogo from './600a41c73b670a97ae1d4f47_Clarifion_Logo 1.png';
 import mcAfee from './McAfee_Secure.png';
 import norton from './norton-antivirus-logo 1.png';
+import completeStep from './step-complete.png';
+import currentStep from './step-current.png';
+import nextStep from './step-next.png';
+import productImage from './product-cover.png';
+import customer from './customer.png';
+import verifiedCustomer from './verified-customer.png';
+import stars from './Stars.png';
+import productSmallImage from './product-small.png';
+import selected from './selected.png';
+import tickCircle from './tick-circle.png';
+import percentVector from './percent-vector.png';
+import lineVector from './Line 1.png';
+import lock from './lock (7) 1.png';
+import creditCards from './credit-cards.png';
+import guarantee from './guarantee.png';
+import whiteLock from './white-lock.png';
 
 export {
-  arrowSync, checkmark, mdiCards, phTruck, clarifionLogo, mcAfee, norton,
+  arrowSync,
+  checkmark,
+  mdiCards,
+  phTruck,
+  clarifionLogo,
+  mcAfee,
+  norton,
+  completeStep,
+  currentStep,
+  nextStep,
+  productImage,
+  customer,
+  verifiedCustomer,
+  stars,
+  productSmallImage,
+  selected,
+  tickCircle,
+  percentVector,
+  lineVector,
+  lock,
+  creditCards,
+  guarantee,
+  whiteLock,
 };
