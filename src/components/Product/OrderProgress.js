6cf66a973e19f05ce149fd9e3eb18de0c35eacec
Amine derbal <aminedoran31@gmail.com>
2023-09-19 19:43:12 +0100

    Implement Mobile Display

--- src/components/Product/OrderProgress.js
+++ src/components/Product/OrderProgress.js
@@ -1,31 +1,58 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { orderProgress } from '../../constants';
 
-const OrderProgress = () => (
-  <div className="mt-10">
-    <div className="flex justify-between">
-      {orderProgress.map((order) => (
-        <div
-          key={order.id}
-          className="flex items-center gap-2"
-        >
-          <img
-            className="w-7 h-7 object-contain"
-            src={order.image}
-            alt={order.name}
-          />
-          <p>
-            step
-            {' '}
-            {order.id}
-            {' '}
-            :
-            {order.name}
-          </p>
-        </div>
-      ))}
+const OrderProgress = () => {
+  const { screenWidth } = useSelector((state) => state.screen);
+
+  return (
+    <div className="mt-10">
+      <div className="flex justify-between">
+        {orderProgress.map((order) => (
+          <div
+            key={order.id}
+            className="flex items-center gap-2"
+          >
+            {screenWidth === 'sm' || screenWidth === 'md' ? (
+              <div className="flex flex-col items-center justify-center gap-2 text-center">
+                <img
+                  className="w-8 h-8 object-contain"
+                  src={order.image}
+                  alt={order.name}
+                />
+                <p>
+                  step
+                  {' '}
+                  {order.id}
+                  {' '}
+                  :
+                  {' '}
+                  {order.name}
+                </p>
+              </div>
+            ) : (
+              <div className="flex items-center gap-2">
+                <img
+                  className="w-8 h-8 object-contain"
+                  src={order.image}
+                  alt={order.name}
+                />
+                <p>
+                  step
+                  {' '}
+                  {order.id}
+                  {' '}
+                  :
+                  {' '}
+                  {order.name}
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default OrderProgress;
