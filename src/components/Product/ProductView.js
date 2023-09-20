f3008c2d85c6c2ea5679772a9885116f77e296ca
Amine derbal <aminedoran31@gmail.com>
2023-09-20 13:53:20 +0100

    Implement Mobile View

--- src/components/Product/ProductView.js
+++ src/components/Product/ProductView.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ProductPreview from './ProductPreview';
 import ProductData from './ProductData';
-// import { customerData } from '../constants';
 
-const ProductView = () => (
-  <div className="p-[5%] flex mt-5 gap-10">
-    <ProductPreview />
-    <ProductData />
-  </div>
-);
+const ProductView = () => {
+  const { screenWidth } = useSelector((state) => state.screen);
+
+  return (
+    <div className="p-[5%] flex mt-5 gap-10">
+      {screenWidth === 'sm' || screenWidth === 'md' ? null : <ProductPreview />}
+      <ProductData />
+    </div>
+  );
+};
 
 export default ProductView;
