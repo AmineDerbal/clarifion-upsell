1cfd3199cabd5b98163d96fb3c5040bdb7686265
Amine derbal <aminedoran31@gmail.com>
2023-09-20 13:29:10 +0100

    Implement Mobile view

--- src/components/Header/HeaderBottom.js
+++ src/components/Header/HeaderBottom.js
@@ -1,16 +1,63 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { norton, clarifionLogo, mcAfee } from '../../assets';
 
-const HeaderBottom = () => (
-  <div className="flex py-5 justify-between px-[5%] ">
-    <div>
-      <img className="w/9 h/9 object-contain" src={clarifionLogo} alt="clarifion logo" />
+const LogoSection = () => (
+  <>
+    <div className="flex items-center">
+      <img
+        className="w-3/5 h-3/5 object-contain"
+        src={clarifionLogo}
+        alt="clarifion logo"
+      />
     </div>
-    <div className="flex gap-4 items-center">
-      <img className="w/9 h/9 object-contain" src={mcAfee} alt="mcAfee logo" />
-      <img className="w/9 h/9 object-contain" src={norton} alt="norton logo" />
+    <div className="flex gap-1 items-center">
+      <img
+        className="w-3/5 h-3/5 object-contain"
+        src={mcAfee}
+        alt="mcAfee logo"
+      />
+      <img
+        className="w-3/5 h-3/5 object-contain"
+        src={norton}
+        alt="norton logo"
+      />
     </div>
-  </div>
+  </>
 );
+
+const HeaderBottom = () => {
+  const { screenWidth } = useSelector((state) => state.screen);
+
+  return (
+    <div className="flex py-5 justify-between px-[5%] ">
+      {screenWidth === 'sm' || screenWidth === 'md' ? (
+        <LogoSection />
+      ) : (
+        <>
+          <div>
+            <img
+              className="object-contain"
+              src={clarifionLogo}
+              alt="clarifion logo"
+            />
+          </div>
+          <div className="flex gap-4 items-center">
+            <img
+              className="object-contain"
+              src={mcAfee}
+              alt="mcAfee logo"
+            />
+            <img
+              className="object-contain"
+              src={norton}
+              alt="norton logo"
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default HeaderBottom;
