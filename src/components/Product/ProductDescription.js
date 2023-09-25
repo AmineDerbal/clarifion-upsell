d3d82c85a3c70d6d1e248357cd9702e3ddbf029d
Amine derbal <aminedoran31@gmail.com>
2023-09-25 17:52:07 +0100

    Update mobile version

--- src/components/Product/ProductDescription.js
+++ src/components/Product/ProductDescription.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'; // Import useState from React
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { productSmallImage, stars } from '../../assets';
+import { productDescription } from '../../constants';
 
 const ProductDescription = () => {
   const [isChecked, setIsChecked] = useState(true); // Define isChecked and toggleCheckbox
@@ -8,53 +10,69 @@ const ProductDescription = () => {
     setIsChecked(!isChecked);
   };
 
-  return (
-    <div className="flex gap-5 w-full">
-      <img
-        src={productSmallImage}
-        alt="clarifion product"
-        className="h-full w-1/3 object-fill"
-      />
-      <div className="flex flex-col justify-between gap-2 w-full">
-        <div className="flex justify-between w-full">
-          <p className="font-semibold text-[20px]">Clarifion Air Ionizer</p>
-          <div className="flex gap-2">
-            <p className="text-gray line-through">$180</p>
-            <p className="text-blue"> $84</p>
-          </div>
-        </div>
-        <img
-          src={stars}
-          alt="number of stars"
-          className="w-1/3 object-fill"
-        />
-        <div className="flex gap-5 items-center">
-          <label
-            htmlFor="myCheckbox"
-            className="flex items-center"
-          >
-            <input
-              type="checkbox"
-              id="myCheckbox"
-              className="hidden"
-              checked={isChecked}
-              onChange={toggleCheckbox}
-            />
-            <div
-              className={`w-6 h-6 flex items-center justify-center border-4 border-bgray rounded-full transition duration-300 ${
-                isChecked ? 'bg-blue' : 'bg-white'
-              }`}
-            />
-          </label>
+  const { screenWidth } = useSelector((store) => store.screen);
 
-          <p className="text-small">12 left in Stock</p>
+  return (
+    <>
+      <div className="flex gap-5 w-full">
+        <img
+          src={productSmallImage}
+          alt="clarifion product"
+          className={
+            screenWidth === 'sm' || screenWidth === 'md'
+              ? 'w-3/5 h-4/5 object-contain'
+              : 'h-full w-1/3 object-fill'
+          }
+        />
+        <div
+          className={
+            screenWidth === 'sm' || screenWidth === 'md'
+              ? 'flex flex-col gap-2 w-full'
+              : 'flex flex-col justify-between gap-2 w-full'
+          }
+        >
+          <div className="flex justify-between w-full">
+            <p className="font-semibold text-[20px]">Clarifion Air Ionizer</p>
+            <div className="flex gap-2">
+              <p className="text-gray line-through">$180</p>
+              <p className="text-blue"> $84</p>
+            </div>
+          </div>
+          <img
+            src={stars}
+            alt="number of stars"
+            className="w-1/3 object-fill"
+          />
+          <div className="flex gap-5 items-center">
+            <label
+              htmlFor="myCheckbox"
+              className="flex items-center"
+            >
+              <input
+                type="checkbox"
+                id="myCheckbox"
+                className="hidden"
+                checked={isChecked}
+                onChange={toggleCheckbox}
+              />
+              <div
+                className={`w-6 h-6 flex items-center justify-center border-4 border-bgray rounded-full transition duration-300 ${
+                  isChecked ? 'bg-blue' : 'bg-white'
+                }`}
+              />
+            </label>
+
+            <p className="text-small">12 left in Stock</p>
+          </div>
+          {screenWidth === 'sm' || screenWidth === 'md' ? null : (
+            <p>{productDescription}</p>
+          )}
         </div>
-        <p>
-          Simply plug a Clarifion into any standard outlet and replace bulky,
-          expensive air purifiers with a simple.
-        </p>
       </div>
-    </div>
+      {screenWidth === 'sm' || screenWidth === 'md' ? (
+        <p className="-mt-6">{productDescription}</p>
+      ) : null}
+    </>
   );
 };
 
